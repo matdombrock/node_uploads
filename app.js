3ea52951d756c863132d0bd8cb5f236e529a60b2
Mathieu Dombrock <matdombrock@gmail.com>
2019-03-26 16:03:59 -0700

    Further Formatting Fix

--- app.js
+++ app.js
@@ -8,23 +8,24 @@ Change this value to match your upload directory!
 const uploadPath = '/home/mathieu/Lab/node_uploads/uploads/';
 
 http.createServer(function (req, res) {
-  if (req.url == '/fileUpload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldPath = files.fileToUpload.path;
-      var newPath = uploadPath + files.fileToUpload.name;
-      fs.rename(oldPath, newPath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileUpload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="fileToUpload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-  }
+    if (req.url == '/fileUpload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            var oldPath = files.fileToUpload.path;
+            var newPath = uploadPath + files.fileToUpload.name;
+            fs.rename(oldPath, newPath, function (err) {
+                if (err) throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+            });
+        });
+    } 
+    else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<form action="fileUpload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="fileToUpload"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+    }
 }).listen(1337);
